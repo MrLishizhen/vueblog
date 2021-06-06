b79ca62043e8f9d3e0b9b02de220a4805bd5fd81
秋雨不良人 <718647063@qq.com>
2021-06-06 20:10:23 +0800

    修改链接参数

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
     devServer: { //配置请求代理
         proxy: {
             '/': { //配置请求代理
-                target: 'http://localhost:3000/'
+                // target: 'http://localhost:3000/'
+                target: 'http://47.94.12.253:3307/'
             }
         }
 
